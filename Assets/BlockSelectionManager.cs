9b7f63d280e02df32094ebe38d9f69a0544c208d
Rob Ambrosius <rob.ambrosius@topicus.nl>
2016-12-16 14:00:34 +0100

    Ander blokje kunnen selecteren toegevoegd

---
+++ Assets/BlockSelectionManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlockSelectionManager : MonoBehaviour
+{
+    public GameObject selectionPanel;
+    public GameObject selectionBlock;
+
+
+    public void ShowBlockSelection()
+    {
+        selectionPanel.SetActive(true);
+        selectionBlock.SetActive(false);
+        Debug.Log("ShowBlockSelection");
+    }
+
+    public void BlockSelect(Sprite selectedImage)
+    {
+        selectionBlock.GetComponent<Image>().sprite = selectedImage;
+
+        selectionPanel.SetActive(false);
+        selectionBlock.SetActive(true);
+        Debug.Log("ShowBlockSelection");
+    }
+}
